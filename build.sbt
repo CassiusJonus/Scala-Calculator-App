f5eab21840b9b80856d1b0a0df07410deacdfbe9
CassiusJonus <vegetafan5@gmail.com>
2021-08-28 19:17:03 -0500

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,7 @@
+name := "Calculator-App"
+
+version := "0.1"
+
+scalaVersion := "3.0.1"
+
+idePackagePrefix := Some("jonus.cassius.calculator")
---
+++ src/main/scala/Main.scala
@@ -0,0 +1,18 @@
+package jonus.cassius.calculator
+
+/**
+ * This object is the entry point of the application.
+ *
+ * @author Cassius Jonus
+ */
+object Main {
+  def main(args: Array[String]): Unit = {
+    // TODO Implement method to launch the calculator
+  }
+}
+
+
+
+
+
+
